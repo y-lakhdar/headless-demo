8b05e3814ea8664ac2fcb5b45e47fc751abbf917
Yassine Lakhdar <ylakhdar@coveo.com>
2021-06-02 21:42:29 -0400

    remove unused components

--- src/Components/SearchPage.tsx
+++ src/Components/SearchPage.tsx
@@ -5,10 +5,6 @@ import Grid from '@material-ui/core/Grid';
 import SearchBox from './SearchBox';
 import QuerySummary from './QuerySummary';
 import ResultList from './ResultList';
-import Pager from './Pager';
-import Sort from './Sort';
-import FacetList from './FacetList';
-import ResultsPerPage from './ResultsPerPage';
 import {AnalyticsActions, SearchActions, Engine} from '@coveo/headless';
 import {EngineProvider} from '../common/engineContext';
 
@@ -37,30 +33,14 @@ const SearchPage: React.FunctionComponent<ISearchPageProps> = (props) => {
 
         <Box my={4}>
           <Grid container>
-            <Grid item md={3} sm={12}>
-              <FacetList />
-            </Grid>
             <Grid item md={9} sm={12}>
               <Box pl={3}>
                 <Grid container alignItems="flex-end">
                   <Grid item md={10}>
                     <QuerySummary />
                   </Grid>
-                  <Grid item md={2}>
-                    <Sort />
-                  </Grid>
                 </Grid>
                 <ResultList />
-              </Box>
-              <Box my={4}>
-                <Grid container>
-                  <Grid item md={6}>
-                    <Pager />
-                  </Grid>
-                  <Grid item md={6}>
-                    <ResultsPerPage />
-                  </Grid>
-                </Grid>
               </Box>
             </Grid>
           </Grid>
